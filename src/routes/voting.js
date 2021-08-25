94bda61a7c0d996769768298ee351239cae1779a
Jainsaheb__ <phagunjain@protonmail.com>
2021-08-25 16:53:58 +0530

    completed arena route logc

--- src/routes/voting.js
+++ src/routes/voting.js
@@ -37,10 +37,8 @@ router.route("/castvote").post(async (req, res) => {
     res.send("YOU MY FREN IS NOT REGISTERED, KINDLY CONTACT THE DEVELOPIER");
   } else {
     if (rs.rows[0].status === "v") {
-      res.render("votingarena", {
-        title: "this is arena",
-        result: "you have casted your vote",
-      });
+      res.send("hello!");
+      res.end();
       // res.send("You have Already Voted");
     }
 
@@ -68,60 +66,19 @@ router.route("/castvote").post(async (req, res) => {
 });
 
 router.route("/arena").get(async (req, res) => {
-  // await client.connect();
-  let sap = req.body.sap;
-  const query = "SELECT * FROM test.candidates";
-  // let rs = await client;
-  //   .execute(query, [sap], {
-  //     prepare: true,
-  //   })
-  //   .catch((error) => {
-  //     console.log("Cant operate ", error);
-  //     res.status(500).json({ error: "Cant add record: " });
-  //   });
+  await client.connect();
 
-  // await client.shutdown();
+  const rs = await client.execute("SELECT * FROM test.candidates;");
+  console.log(rs.rows);
 
-  // if (rs.rowLength === 0) {
-  //   res.send("YOU MY FREN IS NOT REGISTERED, KINDLY CONTACT THE DEVELOPIER");
-  // } else {
-  //   if (rs.rows[0].status === "v") {
-  //     res.render("votingarena", {
-  //       length:
-  //       title: "this is arena",
-  //       result: "you have casted your vote",
-  //     });
-  //     // res.send("You have Already Voted");
-  //   }
-
-  // if (rs.rows[0].status === "n") {
-  //   // await client.execute("UPDATE voting set status='v' where sap=?", [sap], {
-  //   //   prepare: true,
-  //   // });
-  //   res.render("votingarena", {
-  //     title: "this is arena",
-  //     result: "you have not yet casted your vote",
-  //   });
-  // }
-
-  // let rs = await client.execute(query).catch((error) => {
-  //   console.log("Cant operate ", error);
-  //   res.status(500).json({ error: "Cant add" });
-  // });
-
-  res.render("votingarena");
-
-  // }
-
-  // if (rs.rows[0].status === "v") {
-  //   res.send("You have Already Voted");
-  // }
-
-  // if (rs.rows[0].status === "n") {
-  //   res.send("YOU CAN VOTE");
-  // }
-
-  // res.send(rs.rows[0].status);
+  res.render("votingarena", {
+    vote1: rs.rows[0].votes,
+    id1: rs.rows[0].id,
+    name1: rs.rows[0].name,
+    vote2: rs.rows[1].votes,
+    id2: rs.rows[1].id,
+    name2: rs.rows[1].name,
+  });
   res.end();
 });
 
