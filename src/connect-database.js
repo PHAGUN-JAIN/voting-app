8c228f9528f9e321585eea932a24d940ca493311
Jainsaheb__ <phagunjain@protonmail.com>
2021-08-26 10:21:52 +0530

    code cleaning

--- src/connect-database.js
+++ src/connect-database.js
@@ -1,16 +1,14 @@
 require("dotenv").config();
 const { Client } = require("cassandra-driver");
 
-
 async function run() {
   const client = new Client({
     cloud: {
       secureConnectBundle: "./secure-connect-test-database.zip",
     },
     credentials: {
-      username: "XOPLEXSZBsLcimyWlwDeKAqg",
-      password:
-        "8ZxuqYkS8oPoHaMREKOaWQAHwppIXvDz9WP6q+hNpYSr-377p-fYnreXR-RRH-d50TL8iKnXAG28LYy14LSx4CuIl6JEuQlCI-btmgt2I,0JBb9nEZsqHUzTce,urjzn",
+      username: "",
+      password: "",
     },
   });
 
