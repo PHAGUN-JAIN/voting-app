30f8ec35c34a187ed93b79822997a30390231bb2
Jainsaheb__ <phagunjain@protonmail.com>
2021-08-26 10:19:09 +0530

    cleaned code

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,4 @@
 const router = require("express").Router();
-const profileRouter = require("./profile");
-const dashRouter = require("./dashboard");
 const wordRouter = require("./randomizeWord");
 const dataRouter = require("./retriveData");
 const voteRouter = require("./voting");
@@ -8,8 +6,6 @@ router.route("/").get((req, res) => {
   res.send("hello routes");
 });
 
-router.use("/profile", profileRouter);
-router.use("/dashboard", dashRouter);
 router.use("/randomize", wordRouter);
 router.use("/retrive", dataRouter);
 router.use("/voting", voteRouter);
